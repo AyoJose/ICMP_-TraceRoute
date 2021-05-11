cea9b09576f775805f8311acce7633e4696301dd
AyoJose <71681219+AyoJose@users.noreply.github.com>
2021-05-11 01:00:21 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -145,7 +145,7 @@ def get_route(hostname):
                 else:
                     #Fill in start
                     #If there is an exception/error to your if statements, you should append that to your list here
-                    print('error)
+                    print('error')
                     break
                     #Fill in end
          
