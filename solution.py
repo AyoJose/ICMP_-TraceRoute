5c222c28e0909441e0aba552c40ca04fbe3a54ae
AyoJose <71681219+AyoJose@users.noreply.github.com>
2021-05-16 19:14:38 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -168,7 +168,7 @@ def get_route(hostname):
                 tracelist2.extend(tracelist1) #This is your list to contain all traces	
 				
 		#Fill in end
-                    return
+                return
             else:
                     #Fill in start
                     #If there is an exception/error to your if statements, you should append that to your list here
