a9be5fa8892b77b92a81b125ef57b21af92538a1
AyoJose <71681219+AyoJose@users.noreply.github.com>
2021-05-16 20:13:16 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -92,24 +92,25 @@ def get_route(hostname):
             whatReady = select.select([mySocket], [], [], timeLeft)
             print('debug in between') 		
             howLongInSelect =(time.time() - startedSelect)
-            if whatReady[0] == []: # Timeout
+            recvPacket, addr = mySocket.recvfrom(1024)
+            timeReceived = time.time()
+            timeLeft = timeLeft - howLongInSelect
+            if timeLeft <= 0:
+                    #tracelist1.append("* * * Request timed out.")
+                    #Fill in start
                 tracelist1=(tries,'*',"* * * Request timed out.")
-                break		
+                	
+            elif whatReady[0] == []: # Timeout
+                tracelist1=(tries,'*',"* * * Request timed out.")	
                 print('*** Request timed out.') 
                     #Fill in start
                     #tracelist2 = tracelist2.append(tracelist1) 
                     #You should add the list above to your all traces list
                     #Fill in end
-            recvPacket, addr = mySocket.recvfrom(1024)
-            timeReceived = time.time()
-            timeLeft = timeLeft - howLongInSelect
+
                 #print('debug part 2') 
                 #print(timeLeft) 
-            if timeLeft <= 0:
-                    #tracelist1.append("* * * Request timed out.")
-                    #Fill in start
-                 tracelist1=(tries,'*',"* * * Request timed out.")
-                 break
+
 		#print('*** Request timed out.') 
                     #Fill in start
                     #tracelist2 = tracelist2.append(tracelist1) 
@@ -120,62 +121,62 @@ def get_route(hostname):
             #except:
                 #continue
  
-            #else:
-            print('2.9') 
+            else:
+                print('2.9') 
                 #Fill in start
                 #Fetch the icmp type from the IP packet
-            icmp_header = recvPacket[20:28] 
-            type,code,checksum,pid,sequence = struct.unpack('bbHHh', icmp_header) 
+                icmp_header = recvPacket[20:28] 
+                type,code,checksum,pid,sequence = struct.unpack('bbHHh', icmp_header) 
                 #Fill in end
-            try: 
-                host_Name = socket.gethostbyaddr(destAddr)[0] 
-            except: 
-                host_Name = 'hostname not returnable' 
+                try: 
+                    host_Name = socket.gethostbyaddr(destAddr)[0] 
+                except: 
+                    host_Name = 'hostname not returnable' 
                     #Fill in end
 			
-            if type == 11:
-                bytes = struct.calcsize("d")
-                timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                if type == 11:
+                    bytes = struct.calcsize("d")
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here
                     #tracelist1.append('%d , %d, %struct, %s' %(ttl, (timeReceived - timeSent)*1000, addr[0]),host_Name)
-                print('debug part 3 ') 
-                print('%d rtt= %.0f ms %s %s' %(tries, (timeReceived - t)*1000, addr[0],host_Name))
-                tracelist1 = [str(tries),str(timeReceived-t),str(addr[0]),str(host_Name)] #This is your list to use when iterating through each trace 
-                tracelist2.extend(tracelist1) #This is your list to contain all traces	
+                    print('debug part 3 ') 
+                    print('%d rtt= %.0f ms %s %s' %(tries, (timeReceived - t)*1000, addr[0],host_Name))
+                    tracelist1 = [str(tries),str(timeReceived-t),str(addr[0]),str(host_Name)] #This is your list to use when iterating through each trace 
+                    tracelist2.extend(tracelist1) #This is your list to contain all traces	
 		
                     #Fill in end
-            elif type == 3:
-                bytes = struct.calcsize("d")
-                timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                elif type == 3:
+                    bytes = struct.calcsize("d")
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should add your responses to your lists here 
                     #tracelist1.append('%d , %d, %struct, %s' %(ttl, (timeReceived - timeSent)*1000, addr[0]),host_Name)
-                print('debug part 4 ') 
-                print('%d rtt= %.0f ms %s %s' %(tries, (timeReceived - t)*1000, addr[0],host_Name))
-                tracelist1 =[str(tries),str(timeReceived-t),str(addr[0]),str(host_Name)] #This is your list to use when iterating through each trace 
-                tracelist2.extend(tracelist1) #This is your list to contain all traces	
+                    print('debug part 4 ') 
+                    print('%d rtt= %.0f ms %s %s' %(tries, (timeReceived - t)*1000, addr[0],host_Name))
+                    tracelist1 =[str(tries),str(timeReceived-t),str(addr[0]),str(host_Name)] #This is your list to use when iterating through each trace 
+                    tracelist2.extend(tracelist1) #This is your list to contain all traces	
 		          #Fill in end
-            elif type == 0:
-                bytes = struct.calcsize("d")
-                timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                elif type == 0:
+                    bytes = struct.calcsize("d")
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #tracelist1.append('%d , %d, %struct, %s' %(ttl, (timeReceived - timeSent)*1000, addr[0]),host_Name)
                     #You should add your responses to your lists here and return your list if your destination IP is met
-                print('debug part 5 ')
-                print('%d rtt= %.0f ms %s %s' %(tries, (timeReceived - t)*1000, addr[0],host_Name))
-                tracelist1 = [str(tries),str(timeReceived-t),str(addr[0]),str(host_Name)] #This is your list to use when iterating through each trace 
-                tracelist2.extend(tracelist1) #This is your list to contain all traces	
+                    print('debug part 5 ')
+                    print('%d rtt= %.0f ms %s %s' %(tries, (timeReceived - t)*1000, addr[0],host_Name))
+                    tracelist1 = [str(tries),str(timeReceived-t),str(addr[0]),str(host_Name)] #This is your list to use when iterating through each trace 
+                    tracelist2.extend(tracelist1) #This is your list to contain all traces	
 				
 		#Fill in end
-            else:
+                else:
                     #Fill in start
                     #If there is an exception/error to your if statements, you should append that to your list here
-                print('error')
-                print('debug part 6 ') 
-                break
+                    print('error')
+                    print('debug part 6 ') 
+                    break
                     #Fill in end
          
-        #    finally:
-        mySocket.close()
+
+            mySocket.close()
     return tracelist2	
